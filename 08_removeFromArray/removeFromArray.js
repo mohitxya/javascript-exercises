0005194c380720496da4ea2676674895dcd922d6
Mohit <111426594+mohitxya@users.noreply.github.com>
2025-07-24 23:35:51 +0530

    solve 8 and 9

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
 const removeFromArray = function(arr,...key) {
     return arr.filter((num)=>!key.includes(num));
+    // if num is in keys then don't include it.
 };
 
 // Do not edit below this line
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    if(!Number.isInteger(a)||!Number.isInteger(b)) return "ERROR";
+    if(a<0 || b<0) return "ERROR";
+    if(typeof(a)!="number" || typeof(b)!="number") return "ERROR";
+    
+    // Ensure we always iterate from smaller to larger number
+    let min = Math.min(a, b);
+    let max = Math.max(a, b);
+    
+    let sum=0;
+    for(let i=min;i<=max;i++)
+    {
+        sum+=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line

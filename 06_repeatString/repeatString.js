cf1dacc3535ff8195342f69d42155a0060dc8bdf
Mohit <111426594+mohitxya@users.noreply.github.com>
2025-07-24 23:22:09 +0530

    update

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    if (num < 0) return 'ERROR';
+    if (typeof string !== 'string' || typeof num !== 'number') return 'ERROR';
+    let result = '';
+    for (let i = 0; i < num; i++) {
+        result += string;
+    }
+    return result;
 };
 
 // Do not edit below this line
--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let out=string.split('');
+    let final='';
+    let n=out.length;
+    for(let i=n-1;i>=0;i--)
+    {
+        final+=out[i];
+    }
+    return final;
 };
 
 // Do not edit below this line

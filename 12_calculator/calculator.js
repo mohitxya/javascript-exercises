ac91ab8a0b95e6141620ffcc6d059af6d82859d6
Mohit <111426594+mohitxya@users.noreply.github.com>
2025-07-27 23:05:43 +0530

    solved 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let s=0;
+	for(let i of arr)
+  {
+    s+=i;
+  }
+  return s;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, curr) => acc * curr, 1);
 };
 
-const power = function() {
-	
+
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let res=1;
+  for(let i=1;i<=n;i++)
+  {
+    res*=i;
+  }
+  return res;
 };
 
 // Do not edit below this line

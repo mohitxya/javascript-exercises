222ce1494f26e3bf804fd655248ad72a5dc198b9
Mohit <111426594+mohitxya@users.noreply.github.com>
2025-07-27 23:19:58 +0530

    solved 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    //remove any non alpha character
+    const cleaned = str.replace(/[^a-zA-Z]/g, '');
+    const reversed = str.split('').reverse().join('');
 
+    //reverse it and compare
+    if(cleaned==reversed) return true;
+    else return false;
 };
 
 // Do not edit below this line

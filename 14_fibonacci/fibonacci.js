430c2931b4b5e587f73b9bf15ea6beed88003be5
Mohit <111426594+mohitxya@users.noreply.github.com>
2025-07-27 23:33:45 +0530

    solved 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let arr = [0, 1];
+    
+    if (n === 0) return arr[0];
+    else if(n<0) return "OOPS";
+    else if (n === 1) return arr[1];
+    else {
+        for (let i = 2; i <= n; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        return arr[n];
+    }
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;

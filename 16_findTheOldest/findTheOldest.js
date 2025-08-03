696c5f1095e9fa15e78ef0dbb158f2ceadffbcc7
Mohit <111426594+mohitxya@users.noreply.github.com>
2025-08-03 14:17:33 +0530

    minor

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let max = 0;
+    let o = {};
 
+    const currentYear = new Date().getFullYear(); // ✅ Use actual current year
+
+    for (let obj of arr) {
+        let temp = 0;
+
+        if (obj.yearOfDeath !== undefined) {
+            temp = obj.yearOfDeath - obj.yearOfBirth;
+        } else {
+            temp = currentYear - obj.yearOfBirth; // ✅ Not hardcoded
+        }
+
+        if (temp > max) {
+            max = temp;
+            o = obj;
+        }
+    }
+
+    return o;
 };
 
 // Do not edit below this line
